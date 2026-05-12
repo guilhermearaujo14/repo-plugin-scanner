9d56065c496bdb19a84b789843193c6e283d6138
Guilherme Araujo <40188385+guilhermearaujo14@users.noreply.github.com>
2026-05-12 15:55:10 -0300

    Update Scanner.js

--- www/Scanner.js
+++ www/Scanner.js
@@ -1,5 +1,5 @@
 var exec = require('cordova/exec');
 
 exports.scan = function(success, error) {
-    exec(success, error, 'MLKitDocScanner', 'startScan', []);
+    exec(success, error, 'ScannerPlugin', 'startScan', []);
 };
